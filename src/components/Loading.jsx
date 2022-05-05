c1da35bd7a83942d05fc43ab8c82acf1e9030cd9
ceci dip <cecidip41@gmail.com>
2022-05-04 21:05:43 -0300

    dibujo loading

---
+++ src/components/Loading.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../style/loading.css";
+
+import "../style/style.css";
+
+const Loading = () => {
+  return (
+    <div className="pacman">
+      <div className="pacman__mouth"></div>
+      <div className="pacman__food"></div>
+    </div>
+  );
+};
+
+export default Loading;
