642352b895ae1fcb8615fbabd07912e8d73fbbb9
Rober Ousset <robertousset@gmail.com>
2022-05-11 15:05:48 -0300

    Ajustando Layot 1

--- src/components/Perfil.jsx
+++ src/components/Perfil.jsx
@@ -29,54 +29,66 @@ const Perfil = () => {
 					</p>
 				</div>
 			</div>
-			<div className="row justify-content-center">
-				<div className="col-12 col-md-6 col-lg-3 text-center">
-					<button
-						className="btn btn-outline-primary"
-						onClick={mostrarInformacion}
-						style={informacion ? activeStyle : undefined}
-					>
-						Información
-					</button>
-				</div>
-				<div className="col-12 col-md-6 col-lg-3 text-center">
-					<button
-						className="btn btn-outline-success"
-						onClick={mostrarPublicaciones}
-						style={informacion ? undefined : activeStyle}
-					>
-						Publicaciones
-					</button>
-				</div>
-				<div className="col-12 col-md-6 col-lg-3">
-					<div class="card">
-						<img src={cat1} class="card-img-top" alt="..." />
-						<div className="card-body">
-							<h5 className="card-title">Card title</h5>
-							<p className="card-text">
-								Some quick example text to build on the card title and make up
-								the bulk of the card's content.
-							</p>
-						</div>
-						<ul className="list-group list-group-flush">
-							<li className="list-group-item">An item</li>
-							<li className="list-group-item">A second item</li>
-							<li className="list-group-item">A third item</li>
-						</ul>
-						<div className="card-body">
-							<a href="#" className="card-link">
-								Card link
-							</a>
-							<a href="#" className="card-link">
-								Another link
-							</a>
+			<div className="row">
+				<div className="col-12 col-md-8 col-lg-8">
+					<div className="container">
+						<div className="row justify-content-between">
+							<div className="col text-center">
+								<button
+									className="btn btn-outline-primary"
+									onClick={mostrarInformacion}
+									style={informacion ? activeStyle : undefined}
+								>
+									Información
+								</button>
+							</div>
+							<div className="col text-center">
+								<button
+									className="btn btn-outline-success"
+									onClick={mostrarPublicaciones}
+									style={informacion ? undefined : activeStyle}
+								>
+									Publicaciones
+								</button>
+							</div>
+							<div className="row">
+								<div className="col">
+									{informacion ? <Perfil_Info /> : <Perfil_Publish />}
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-			<div className="row">
-				<div className="col">
-					{informacion ? <Perfil_Info /> : <Perfil_Publish />}
+				<div className="col-12 col-md-4 col-lg-4 d-none d-md-block d-lg-block">
+					<div className="container">
+						<div className="row">
+							<div className="col">
+								<div class="card">
+									<img src={cat1} class="card-img-top" alt="..." />
+									<div className="card-body">
+										<h5 className="card-title">Card title</h5>
+										<p className="card-text">
+											Some quick example text to build on the card title and
+											make up the bulk of the card's content.
+										</p>
+									</div>
+									<ul className="list-group list-group-flush">
+										<li className="list-group-item">An item</li>
+										<li className="list-group-item">A second item</li>
+										<li className="list-group-item">A third item</li>
+									</ul>
+									<div className="card-body">
+										<a href="#" className="card-link">
+											Card link
+										</a>
+										<a href="#" className="card-link">
+											Another link
+										</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
