ae799ed1c8402740193fb9f11c7bcec551ed8a10
ceci dip <cecidip41@gmail.com>
2022-05-16 20:52:58 -0300

    agrego columnas footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "../style/footer.css";
 
@@ -6,10 +7,35 @@ const Footer = () => {
   return (
     <div className="card footer-card">
       <div className="card-body text-muted">
-        {/* agregar elementos al footer que creamos necesarios */}
-        <div className="col-12">©2022 GamerCat</div>
-        <div className="col-12">©2022 GamerCat</div>
-        <div className="col-12">©2022 GamerCat</div>
+        <div className="col-12">
+          {/* <Link to="*"> */}
+          <i class="bi bi-facebook"></i>
+          {/* </Link>  */}
+          {/* <Link to="*"> */}
+          <i class="bi bi-instagram"></i>
+          {/* </Link>  */}
+          {/* <Link to="*"> */}
+          <i class="bi bi-twitter"></i>
+          {/* </Link>  */}
+          {/* <Link to="*"> */}
+          <i class="bi bi-github"></i>
+          {/* </Link>  */}
+          {/* <Link to="*"> */}
+          <i class="bi bi-discord"></i>
+          {/* </Link>  */}
+          {/* <Link to="*"> */}
+          <i class="bi bi-slack"></i>
+          {/* </Link>  */}
+        </div>
+        {/* <Link to="*"> */}
+        <div className="col-12">About us</div>
+        {/* </Link>  */}
+        {/* <Link to="*"> */}
+        <div className="col-12">Privacy policy</div>
+        {/* </Link>  */}
+        {/* <Link to="*"> */}
+        <div className="col-12">Support us!</div>
+        {/* </Link>  */}
         <div className="col-12">©2022 GamerCat</div>
       </div>
     </div>
