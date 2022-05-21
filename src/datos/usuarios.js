796289f78adb51def367d302c308c0d941167830
Rober Ousset <robertousset@gmail.com>
2022-05-20 23:54:48 -0300

    generando page Admin

---
+++ src/datos/usuarios.js
@@ -0,0 +1,65 @@
+// datos hard codeados para hacer una prueba de la vista.
+import cat1 from "../img/cat1.png";
+import cat2 from "../img/cat2.png";
+import cat3 from "../img/cat3.png";
+import cat4 from "../img/cat4.png";
+import cat5 from "../img/cat5.png";
+
+export const usuarios = [
+	{
+		id: 1,
+		email: "prueba1@prueba.com",
+		first_name: "Juan",
+		last_name: "Alejandro",
+		avatar: cat1,
+		skill: "backend developer",
+		linkedIn: "User LinkedIn",
+		gitUser: "Git User Name",
+		description: "texto de prueba",
+	},
+
+	{
+		id: 2,
+		email: "prueba1@prueba.com",
+		first_name: "Miqueas",
+		last_name: "Acevedo",
+		avatar: cat2,
+		skill: "backend developer",
+		linkedIn: "User LinkedIn",
+		gitUser: "Git User Name",
+		description: "texto de prueba",
+	},
+	{
+		id: 3,
+		email: "prueba2@prueba.com",
+		first_name: "Cecy",
+		last_name: "Dip",
+		avatar: cat3,
+		skill: "Frontend Developer",
+		linkedIn: "User LinkedIn",
+		gitUser: "Git User Name",
+		description: "texto de prueba",
+	},
+	{
+		id: 4,
+		email: "prueba1@prueba.com",
+		first_name: "Mauro",
+		last_name: "Soto",
+		avatar: cat5,
+		skill: "Frontend Devoloper",
+		linkedIn: "User LinkedIn",
+		gitUser: "Git User Name",
+		description: "texto de prueba",
+	},
+	{
+		id: 4,
+		email: "prueba1@prueba.com",
+		first_name: "Rober",
+		last_name: "Ousset",
+		avatar: cat4,
+		skill: "Frontend Devoloper",
+		linkedIn: "User LinkedIn",
+		gitUser: "Git User Name",
+		description: "texto de prueba",
+	},
+];
