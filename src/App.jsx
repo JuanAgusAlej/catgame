ed91d93b9fe0e3585a9152d43723fc99e3555ba6
Rober Ousset <robertousset@gmail.com>
2022-05-28 16:53:27 -0300

    creando rutas protegidas

--- src/App.jsx
+++ src/App.jsx
@@ -16,19 +16,27 @@ import Perfil from "./pages/Perfil";
 import RegScreen from "./pages/RegScreen";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ProtectedRoutes from "./components/ProtectedRoutes";
 
 const App = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<LoginScreen />} />
-				<Route path="/" element={<Feed />} />
-				<Route path="/aboutus" element={<AboutUs />} />
-				<Route path="/perfil" element={<Perfil />} />
-				<Route path="/admin" element={<Admin />} />
-				<Route path="/register" element={<RegScreen />} />
-				<Route path="/password" element={<Contraseña />} />
-				<Route path="/noticias" element={<CardNoticias />} />
+				<Route path="login" element={<LoginScreen />} />
+				<Route
+					path="/"
+					element={
+						<ProtectedRoutes>
+							<Feed />
+						</ProtectedRoutes>
+					}
+				/>
+				<Route path="aboutus" element={<AboutUs />} />
+				<Route path="perfil" element={<Perfil />} />
+				<Route path="admin" element={<Admin />} />
+				<Route path="/egister" element={<RegScreen />} />
+				<Route path="password" element={<Contraseña />} />
+				<Route path="noticias" element={<CardNoticias />} />
 				<Route path="*" element={<Error404 />} />
 			</Routes>
 		</BrowserRouter>
