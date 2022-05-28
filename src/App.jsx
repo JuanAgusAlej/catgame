acc957d6f5dc562b43765e591085d5f6655ef5a9
Rober Ousset <robertousset@gmail.com>
2022-05-28 16:01:18 -0300

    creando las rutas

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,35 @@ import Navbar from "./components/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import "bootstrap/dist/js/bootstrap.bundle";
+
+import AboutUs from "./pages/AboutUs";
 import Admin from "./pages/Admin";
+import CardNoticias from "./pages/CardNoticias";
+import Contraseña from "./pages/Contraseña";
+import Error404 from "./pages/Error404";
+import Feed from "./pages/Feed";
+import LoginScreen from "./pages/LoginSreen";
+import Perfil from "./pages/Perfil";
+import RegScreen from "./pages/RegScreen";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
-	// return <Admin />;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<LoginScreen />} />
+				<Route path="/" element={<Feed />} />
+				<Route path="/aboutus" element={<AboutUs />} />
+				<Route path="/perfil" element={<Perfil />} />
+				<Route path="/admin" element={<Admin />} />
+				<Route path="/register" element={<RegScreen />} />
+				<Route path="/password" element={<Contraseña />} />
+				<Route path="/noticias" element={<CardNoticias />} />
+				<Route path="*" element={<Error404 />} />
+			</Routes>
+		</BrowserRouter>
+	);
 };
 
 export default App;
