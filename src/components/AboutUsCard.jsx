d5ac5327c5c03748ccb572a6cd6f78e5d49cd1b6
ceci dip <cecidip41@gmail.com>
2022-06-03 13:20:35 -0300

    estilo about us

--- src/components/AboutUsCard.jsx
+++ src/components/AboutUsCard.jsx
@@ -25,7 +25,7 @@ const AboutUsCard = () => {
     <React.Fragment>
       {infoNosotros.datos.map((datos, index) => (
         <div key={index} className="col col-md-4 col-sm-12 mx-2 mt-2">
-          <div className="card mb-3 w-100 h-100">
+          <div className="card card-us mb-3 w-100 h-100">
             <img
               src={datos.imagen}
               className="card-img-top rounded mx-auto d-block  redondo card-avatar-about"
@@ -33,23 +33,26 @@ const AboutUsCard = () => {
             />
             <div className="card-body text-center">
               <p className="card-text fs-4">{datos.name}</p>
-              <i className="bi bi-envelope mx-1"></i>
-              <p className="card-text text-email fw-lighter">{datos.email}</p>
+              <p className="card-text text-email fw-light">
+                <i className="bi bi-envelope mx-1"></i>
+                {datos.email}
+              </p>
               <p
                 className={
                   datos.skill[0] === "BackEnd"
-                    ? "badge bg-success text-wrap"
-                    : "badge bg-warning text-black text-wrap"
+                    ? "badge bg-light text-black text-wrap"
+                    : "badge bg-light text-black text-wrap"
                 }
               >
                 {datos.skill[0]}
               </p>
-              <p className="badge bg-info text-wrap ms-1">{datos.skill[1]}</p>
-              <p className="fs-6 fw-lighter">
+              <p className="badge bg-light text-black text-wrap">
+                {datos.skill[1]}
+              </p>
+              <p className="text-end">
                 {" "}
-                Linked
-                <a href={datos.linkedin}>
-                  <i className="bi bi-linkedin" style={{ fontSize: 20 }}></i>
+                <a href={datos.linkedin} target="_blank">
+                  <i className="bi bi-linkedin"></i>
                 </a>
               </p>
               {/* <p className="fs-6">{datos.gitUser}</p> */}
--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -3,16 +3,14 @@ import AboutUsCard from "../components/AboutUsCard";
 
 const AboutUs = () => {
   return (
-    <div className="container">
+    <div className="container container-aboutus">
       <div className="row">
         <div className="col">
-          <h2 className="fs-1 fst-italic text-center mb-5 border rounded-pill border-warning border-3">
-            About Us
-          </h2>
+          <h2 className="mb-5 tittle">About Us</h2>
         </div>
-        <div className="row justify-content-center">
-          <AboutUsCard />
-        </div>
+      </div>
+      <div className="row justify-content-center">
+        <AboutUsCard />
       </div>
     </div>
   );
